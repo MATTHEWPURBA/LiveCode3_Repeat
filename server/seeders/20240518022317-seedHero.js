ffba8b18f1c687f848f0b44eff6c10a5ca93b491
Robherto <robhertomatt@gmail.com>
2024-05-18 10:52:58 +0700

    seed hero and user done

--- server/seeders/20240518022317-seedHero.js
+++ server/seeders/20240518022317-seedHero.js
@@ -3,10 +3,18 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-  
+    const dataHero = require("../data/heroes.json").map((el) => {
+      el.createdAt = new Date();
+      el.updatedAt = new Date();
+      return el;
+    });
+    await queryInterface.bulkInsert("Heros", dataHero, {});
   },
 
   async down(queryInterface, Sequelize) {
-  
+    await queryInterface.bulkDelete("Heros", null, {
+      truncate: true,
+      restartIdentity: true,
+    });
   },
 };
